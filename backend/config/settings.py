4d18da8c0d07a0085277325fb36f281219c2f861
SeungHyuk_0276 <105186530+SeungHyuk-Kookmin@users.noreply.github.com>
2026-03-18 11:29:13 +0900

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -242,7 +242,7 @@ REST_FRAMEWORK = {
 
 # Simple JWT 설정
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(hours=2),
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=24),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
