358ef0bd89231ce20a7fef4183bcd84ba5e6fbb0
seunghyuk <chungii1@gmail.com>
2026-03-20 19:55:10 +0900

    fix error

--- frontend/src/app/(auth)/register/page.tsx
+++ frontend/src/app/(auth)/register/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import Logo from "@/shared/components/layout/Logo";
+import LoginLogo from "@/shared/components/layout/LoginLoGo";
 
 export default function RegisterPage() {
   return (
